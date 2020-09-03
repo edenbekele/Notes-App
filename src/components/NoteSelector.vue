4b0cfc9b1ba4d827bfa126683fe74047e8412f13
EDEN <EDBEK@YAHOO.COM>
2020-09-02 20:04:11 -0500

    Created subcomponents and JS ability to hightlight only notes that are selected

---
+++ src/components/NoteSelector.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="note-selector" v-bind:class="{active: selectedNoteId === note.id}" v-on:click="selectNote(note)">
+      <p class="note-selector-title">{{ note.body }}</p>
+      <p class="note-selector-timestamp">{{ note.timestamp }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "note-selector",
+  props: ["note", "selectedNoteId"],
+  methods: {
+    selectNote: function(note) {
+      console.log("This is the selected note", note, note.id);
+      this.$emit("selectNote", note);
+    },
+  },
+};
+</script>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="note-selectors">
+    <NoteSelector 
+      v-for="note in notes" 
+      v-bind:note="note" 
+      v-bind:selectedNoteId="selectedNoteId" 
+      v-on:selectNote="selectNote"
+      v-bind:key="note.id" 
+      />
+  </div>
+</template>
+
+<script>
+import NoteSelector from "./NoteSelector";
+
+export default {
+  name: "note-selectors",
+  components: {
+    NoteSelector,
+  },
+  data: function() {
+    return {
+      notes: [
+        { id: 1, body: "This is the first note", timestamp: Date.now() },
+        { id: 2, body: "This is the second note", timestamp: Date.now() },
+        { id: 3, body: "This is the third note", timestamp: Date.now() },
+        { id: 4, body: "This is the fourth note", timestamp: Date.now() },
+      ],
+      selectedNoteId: 2,
+    };
+  },
+  methods: {
+    selectNote: function(note) {
+      this.selectedNoteId = note.id;
+    },
+  },
+};
+</script>
